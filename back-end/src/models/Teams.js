d9cc9ecf8a1a361ffa76c582a531e395cb2a330f
danicelistobon <danielct-97@hotmail.com>
2020-07-29 20:45:19 -0500

    Remove unique field

--- back-end/src/models/Teams.js
+++ back-end/src/models/Teams.js
@@ -5,16 +5,16 @@ const teamsSchema = new Schema({
   teamName: { type: String, trim: true, required: true },
   whyYourTeam: { type: String, trim: true, required: true },
   memberOneName: { type: String, trim: true, required: true },
-  memberOneGithub: { type: String, trim: true, required: true, unique: true },
-  memberOneLinkedin: { type: String, trim: true, required: true, unique: true },
+  memberOneGithub: { type: String, trim: true, required: true },
+  memberOneLinkedin: { type: String, trim: true, required: true },
   memberOneDone: { type: String, trim: true, required: true },
   memberTwoName: { type: String, trim: true },
-  memberTwoGithub: { type: String, trim: true, unique: true },
-  memberTwoLinkedin: { type: String, trim: true, unique: true },
+  memberTwoGithub: { type: String, trim: true },
+  memberTwoLinkedin: { type: String, trim: true },
   memberTwoDone: { type: String, trim: true },
   memberThreeName: { type: String, trim: true },
-  memberThreeGithub: { type: String, trim: true, unique: true },
-  memberThreeLinkedin: { type: String, trim: true, unique: true },
+  memberThreeGithub: { type: String, trim: true },
+  memberThreeLinkedin: { type: String, trim: true },
   memberThreeDone: { type: String, trim: true }
 });
 
